91649577417a818533fc4538730111852445d985
dizeroh <dizer.oh@gmail.com>
2016-09-11 10:48:42 -0700

    replaced cmd runner with latest version

--- LoadtestRepo/reports/perfpagegraphs.sh
+++ LoadtestRepo/reports/perfpagegraphs.sh
@@ -10,7 +10,7 @@ array=( ThreadsStateOverTime TimesVsThreads TransactionsPerSecond )
 for aggGraph in "${array[@]}"
 do
         echo "Aggregate Graph - ${aggGraph}"
-        java -jar ${JMTR_PATH}/lib/ext/cmdrunner-1.0.2.jar --tool Reporter --generate-png ${reports_location}reports/images/Aggr-${aggGraph}.png --input-jtl  ${logs_location}/${jmeter_resultsFile} --plugin-type ${aggGraph} --width $graphs_width_Pixels --height $graphs_height_Pixels --aggregate-rows yes --paint-gradient no --paint-zeroing yes --paint-markers yes
+        java -jar ${JMTR_PATH}/lib/ext/CMDRunner.jar --tool Reporter --generate-png ${reports_location}reports/images/Aggr-${aggGraph}.png --input-jtl  ${logs_location}/${jmeter_resultsFile} --plugin-type ${aggGraph} --width $graphs_width_Pixels --height $graphs_height_Pixels --aggregate-rows yes --paint-gradient no --paint-zeroing yes --paint-markers yes
 done
 
 #Independent graph names
@@ -20,7 +20,7 @@ array=( ThreadsStateOverTime BytesThroughputOverTime HitsPerSecond LatenciesOver
 for graphName in "${array[@]}"
 do
         echo "Graph - ${graphName}"
-        java -jar ${JMTR_PATH}/lib/ext/cmdrunner-1.0.2.jar --tool Reporter --generate-png ${reports_location}reports/images/${graphName}.png --input-jtl  ${logs_location}/${jmeter_resultsFile} --plugin-type ${graphName} --width $graphs_width_Pixels --height $graphs_height_Pixels --aggregate-rows no --paint-gradient no --paint-zeroing yes --paint-markers yes
+        java -jar ${JMTR_PATH}/lib/ext/CMDRunner.jar --tool Reporter --generate-png ${reports_location}reports/images/${graphName}.png --input-jtl  ${logs_location}/${jmeter_resultsFile} --plugin-type ${graphName} --width $graphs_width_Pixels --height $graphs_height_Pixels --aggregate-rows no --paint-gradient no --paint-zeroing yes --paint-markers yes
 done
 
 #Generate csv ${reports_location}reports
